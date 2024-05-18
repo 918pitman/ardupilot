88bad301fc1f2cb64a5d7eaab9a6d5ad15986735
918pitman <918pitman@gmail.com>
2024-05-18 18:55:09 -0500

    Update AP_SWIVEL.cpp

--- libraries/AP_SWIVEL/AP_SWIVEL.cpp
+++ libraries/AP_SWIVEL/AP_SWIVEL.cpp
@@ -47,7 +47,7 @@ void AP_SWIVEL::update(void)
         _measurement = _measurement + 0.1;
     }
     else {
-        _measurement = 1.111
+        _measurement = 1.111;
         //_measurement = rssi_analog_source->read_latest();
     }
 
