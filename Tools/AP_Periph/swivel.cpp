f265f66a2cdd745d0a3153f1511bf4f798f31ba1
918pitman <918pitman@gmail.com>
2024-05-19 18:57:08 -0500

    Update swivel.cpp

--- Tools/AP_Periph/swivel.cpp
+++ Tools/AP_Periph/swivel.cpp
@@ -16,7 +16,7 @@ void AP_Periph_FW::can_swivel_update()
 
     uint32_t now = AP_HAL::millis();
     static uint32_t last_update_ms;
-    if (now - last_update_ms < 100) {
+    if (now - last_update_ms < 20) {
         return;
     }
     last_update_ms = now;
@@ -34,23 +34,10 @@ void AP_Periph_FW::can_swivel_update()
     uint16_t total_size = uavcan_equipment_actuator_Status_encode(&pkt, buffer, !periph.canfdout());
 
     canard_broadcast(UAVCAN_EQUIPMENT_ACTUATOR_STATUS_SIGNATURE,
-                    UAVCAN_EQUIPMENT_ACTUATOR_STATUS_ID,
-                    CANARD_TRANSFER_PRIORITY_LOW,
-                    &buffer[0],
-                    total_size);
-
-    // ardupilot_equipment_feedback_SteerAngle pkt {};
-
-    // pkt.steer_angle = swivel.get_angle();
-
-    // uint8_t buffer[ARDUPILOT_EQUIPMENT_FEEDBACK_STEERANGLE_MAX_SIZE] {};
-    // uint16_t total_size = ardupilot_equipment_feedback_SteerAngle_encode(&pkt, buffer, !periph.canfdout());
-
-    // canard_broadcast(ARDUPILOT_EQUIPMENT_FEEDBACK_STEERANGLE_SIGNATURE,
-    //                  ARDUPILOT_EQUIPMENT_FEEDBACK_STEERANGLE_ID,
-    //                  CANARD_TRANSFER_PRIORITY_LOW,
-    //                  &buffer[0],
-    //                  total_size);
+                     UAVCAN_EQUIPMENT_ACTUATOR_STATUS_ID,
+                     CANARD_TRANSFER_PRIORITY_LOW,
+                     &buffer[0],
+                     total_size);
 }
 
 
