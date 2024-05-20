1a8aba8dccd2a86715f461e7c9dafe0ed8e689d2
918pitman <918pitman@gmail.com>
2024-05-20 13:58:29 -0500

    Update system.cpp

--- Rover/system.cpp
+++ Rover/system.cpp
@@ -80,9 +80,9 @@ void Rover::init_ardupilot()
     g2.beacon.init();
 #endif
 
-#if AP_SWIVEL_ENABLED
-    g2.swivel.init();
-#endif
+// #if AP_SWIVEL_ENABLED
+//     g2.swivel.init();
+// #endif
 
     // and baro for EKF
     barometer.set_log_baro_bit(MASK_LOG_IMU);
