80057d308b64f131d8093eaea8c3545ee2257fee
918pitman <918pitman@gmail.com>
2024-05-22 15:31:05 -0500

    Update AP_SWIVEL_Backend.h

--- libraries/AP_SWIVEL/AP_SWIVEL_Backend.h
+++ libraries/AP_SWIVEL/AP_SWIVEL_Backend.h
@@ -15,8 +15,8 @@ public:
     virtual void update() = 0;
 
     int8_t get_pin(void) const { return ap_swivel._params.pin.get(); }
-    float get_v_min(void) { return ap_swivel._params.volt_min.get(); }
-    float get_v_max(void) { return ap_swivel._params.volt_max.get(); }
+    float get_v_min(void) const { return ap_swivel._params.volt_min.get(); }
+    float get_v_max(void) const { return ap_swivel._params.volt_max.get(); }
 
 protected:
 
