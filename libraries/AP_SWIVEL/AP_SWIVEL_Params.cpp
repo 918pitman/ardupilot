920a5b0524bcca68b5dede57a82311389475f660
918pitman <918pitman@gmail.com>
2024-05-22 16:24:21 -0500

    Update AP_SWIVEL_Params.cpp

--- libraries/AP_SWIVEL/AP_SWIVEL_Params.cpp
+++ libraries/AP_SWIVEL/AP_SWIVEL_Params.cpp
@@ -3,32 +3,32 @@
 // table of user settable parameters
 const AP_Param::GroupInfo AP_SWIVEL_Params::var_info[] = {
     // @Param: TYPE
-    // @DisplayName: SWIVEL type
-    // @Description: What type of SWIVEL sensor is connected
+    // @DisplayName: sensor type
+    // @Description: What type of sensor is connected
     // @Values: 0:None,1:Analog,2:DroneCAN
     // @User: Standard
     AP_GROUPINFO_FLAGS("TYPE", 1, AP_SWIVEL_Params, type, 0, AP_PARAM_FLAG_ENABLE),
 
     // @Param: PIN
-    // @DisplayName: Input pin number
+    // @DisplayName: Input pin
     // @Description: Analog Pin to measure angle from
     // @Values: -1:Disabled,50:AUX1,51:AUX2,52:AUX3,53:AUX4,54:AUX5,55:AUX6
     // @User: Standard
     AP_GROUPINFO("PIN", 2, AP_SWIVEL_Params, pin, 8),
 
-    // @Param: VOLT_MIN
-    // @DisplayName: Input pin number
-    // @Description: Analog Pin to measure angle from
+    // @Param: MIN
+    // @DisplayName: Minimum voltage
+    // @Description: Minimum voltage read when locked either cw/ccw
     // @Units: V
     // @User: Standard
-    AP_GROUPINFO("VOLT_MIN", 3, AP_SWIVEL_Params, volt_min, 0.0f),
+    AP_GROUPINFO("MIN", 3, AP_SWIVEL_Params, volt_min, 0),
 
-    // @Param: VOLT_MAX
-    // @DisplayName: Input pin number
-    // @Description: Analog Pin to measure angle from
+    // @Param: MAX
+    // @DisplayName: Maximum voltage
+    // @Description: Maximum voltage read when locked either cw/ccw
     // @Units: V
     // @User: Standard
-    AP_GROUPINFO("VOLT_MAX", 4, AP_SWIVEL_Params, volt_max, 3.3f),
+    AP_GROUPINFO("MAX", 4, AP_SWIVEL_Params, volt_max, 3.3f),
 
     AP_GROUPEND
 };
