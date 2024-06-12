cec6bff7c59ad73ef22de8f7330c60e5307f39d2
918pitman <918pitman@gmail.com>
2024-06-12 13:05:44 -0500

    Update AP_MotorsUGV.cpp

--- libraries/AR_Motors/AP_MotorsUGV.cpp
+++ libraries/AR_Motors/AP_MotorsUGV.cpp
@@ -718,53 +718,51 @@ void AP_MotorsUGV::output_regular(bool armed, float ground_speed, float steering
         if (_scale_steering) {
             // vectored thrust handling
             if (have_vectored_thrust()) {
-                // Pivot turning with vectored thrust
-                if (fabsf(steering) > 300.0f && is_zero(throttle)) {
-                    throttle = fabsf(steering * 100 / 4500.0f);
-                    steering = is_positive(steering) ? 4500.0f : -4500.0f;
-                } else {
-                    // normalise desired steering and throttle to ease calculations
-                    float steering_norm = steering / 4500.0f;
-                    const float throttle_norm = throttle * 0.01f;
+                // normalise desired steering and throttle to ease calculations
+                float steering_norm = steering / 4500.0f;
+                const float throttle_norm = throttle * 0.01f;
 
-                    // steering can never be more than throttle * tan(_vector_angle_max)
-                    const float vector_angle_max_rad = radians(constrain_float(_vector_angle_max, 0.0f, 90.0f));
-                    const float steering_norm_lim = fabsf(throttle_norm * tanf(vector_angle_max_rad));
-                    if (fabsf(steering_norm) > steering_norm_lim) {
-                        if (is_positive(steering_norm)) {
-                            steering_norm = steering_norm_lim;
-                        }
-                        if (is_negative(steering_norm)) {
-                            steering_norm = -steering_norm_lim;
-                        }
+                // steering can never be more than throttle * tan(_vector_angle_max)
+                const float vector_angle_max_rad = radians(constrain_float(_vector_angle_max, 0.0f, 90.0f));
+                const float steering_norm_lim = fabsf(throttle_norm * tanf(vector_angle_max_rad));
+                if (fabsf(steering_norm) > steering_norm_lim) {
+                    if (is_positive(steering_norm)) {
+                        steering_norm = steering_norm_lim;
+                    }
+                    if (is_negative(steering_norm)) {
+                        steering_norm = -steering_norm_lim;
+                    }
+                    limit.steer_right = true;
+                    limit.steer_left = true;
+                }
+
+                if (!is_zero(throttle_norm)) {
+                    // calculate steering angle
+                    float steering_angle_rad = atanf(steering_norm / throttle_norm);
+                    // limit steering angle to vector_angle_max
+                    if (fabsf(steering_angle_rad) > vector_angle_max_rad) {
+                        steering_angle_rad = constrain_float(steering_angle_rad, -vector_angle_max_rad, vector_angle_max_rad);
                         limit.steer_right = true;
                         limit.steer_left = true;
                     }
 
-                    if (!is_zero(throttle_norm)) {
-                        // calculate steering angle
-                        float steering_angle_rad = atanf(steering_norm / throttle_norm);
-                        // limit steering angle to vector_angle_max
-                        if (fabsf(steering_angle_rad) > vector_angle_max_rad) {
-                            steering_angle_rad = constrain_float(steering_angle_rad, -vector_angle_max_rad, vector_angle_max_rad);
-                            limit.steer_right = true;
-                            limit.steer_left = true;
-                        }
+                    // convert steering angle to steering output
+                    steering = steering_angle_rad / vector_angle_max_rad * 4500.0f;
 
-                        // convert steering angle to steering output
-                        steering = steering_angle_rad / vector_angle_max_rad * 4500.0f;
-
-                        // scale up throttle to compensate for steering angle
-                        float throttle_scaler_inv = 1;
-                        if (have_swivel_steering()) {
-                            throttle_scaler_inv = cosf(_swivel_angle);
-                        } else {
-                            throttle_scaler_inv = cosf(steering_angle_rad);
-                        }
-                        if (!is_zero(throttle_scaler_inv)) {
-                            throttle /= throttle_scaler_inv;
-                        }
+                    // scale up throttle to compensate for steering angle
+                    float throttle_scaler_inv = 1;
+                    if (have_swivel_steering()) {
+                        throttle_scaler_inv = cosf(_swivel_angle);
+                    } else {
+                        throttle_scaler_inv = cosf(steering_angle_rad);
                     }
+                    if (!is_zero(throttle_scaler_inv)) {
+                        throttle /= throttle_scaler_inv;
+                    }
+                } else {
+                    // Pivot turning with vectored thrust
+                    throttle = fabsf(steering * 100 / 4500.0f);
+                    steering = is_positive(steering) ? 4500.0f : -4500.0f;
                 }
             } else {
                 // scale steering down as speed increase above MOT_SPD_SCA_BASE (1 m/s default)
